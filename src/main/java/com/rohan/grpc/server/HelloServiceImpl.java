927109b4169dfc87c7f8fb99559b5895f8e16aff
Rohan <rohan17091999@gmail.com>
2021-02-28 21:02:16 +0530

    Initial commit

---
+++ src/main/java/com/rohan/grpc/server/HelloServiceImpl.java
@@ -0,0 +1,29 @@
+package com.rohan.grpc.server;
+
+import com.rohan.grpc.HelloRequest;
+import com.rohan.grpc.HelloResponse;
+import com.rohan.grpc.HelloServiceGrpc.HelloServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceImplBase {
+
+    @Override
+    public void hello(
+      HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println("Request received from client:\n" + request);
+
+        String greeting = new StringBuilder().append("Hello, ")
+            .append(request.getFirstName())
+            .append(" ")
+            .append(request.getLastName())
+            .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+            .setGreeting(greeting)
+            .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
